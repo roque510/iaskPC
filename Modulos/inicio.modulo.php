a4442bfe22f1386049ab38fb90c93ee1adbf9d42
Armando roque barahona <Armando roque barahona>
2016-10-16 12:55:18 -0600

    update front-end guias

--- Modulos/inicio.modulo.php
+++ Modulos/inicio.modulo.php
@@ -2,7 +2,7 @@
   <div class="row infowrap">
     <div class="col s12">
         <div class="info">
-          <h1 class="white-text">Guias</h1>
+          <h1 class="white-text">Guias <span>|</span><span class="crear">Crear Guia</span></h1>
         </div>    
     </div>
   </div>
@@ -10,19 +10,41 @@
 
   <ul class="collapsible borderless z-depth-0 transparent" data-collapsible="accordion">
     <li>
-      <div class="collapsible-header borderless transparent white-text"><i class="material-icons">filter_drama</i>First <div class="right" style="margin-right:20px;">Estatus: <span style="margin-left:10px;" class="right"> Activo</span></div>
+      <div class="collapsible-header borderless transparent white-text shadow"><i class="material-icons">filter_drama</i>First 
 
-      <div class="right" style="margin-right:20px;">Porcentaje: <span style="margin-left:10px;" class="right"> 0%</span></div>
+      <div class="right"><i class="material-icons stroke edit">mode_edit</i><i class="material-icons stroke delete">delete</i></div>
+
+      <div class="right pdefault shadow" style="margin-right:20px;">Estatus: <span style="margin-left:10px;" class="right"> Activo  </span></div>
+
+      <div class="right pdefault shadow" style="margin-right:20px;">Porcentaje: <span style="margin-left:10px;" class="right"> 0%</span> </div>
+
+
+      
+      </div>
+      <div class="collapsible-body colab white-text"><p>Lorem ipsum dolor sit amet.</p></div>
+    </li>
+    <li>
+      <div class="collapsible-header borderless transparent white-text shadow"><i class="material-icons">place</i>Second
+      
+            <div class="right"><i class="material-icons stroke edit">mode_edit</i><i class="material-icons stroke delete">delete</i></div>
+
+      <div class="right pdefault shadow" style="margin-right:20px;">Estatus: <span style="margin-left:10px;" class="right"> Activo  </span></div>
+
+      <div class="right pdefault shadow" style="margin-right:20px;">Porcentaje: <span style="margin-left:10px;" class="right"> 0%</span> </div>
 
       </div>
       <div class="collapsible-body colab white-text"><p>Lorem ipsum dolor sit amet.</p></div>
     </li>
     <li>
-      <div class="collapsible-header borderless transparent white-text"><i class="material-icons">place</i>Second</div>
-      <div class="collapsible-body colab white-text"><p>Lorem ipsum dolor sit amet.</p></div>
-    </li>
-    <li>
-      <div class="collapsible-header borderless transparent white-text"><i class="material-icons">whatshot</i>Third</div>
+      <div class="collapsible-header borderless transparent white-text shadow"><i class="material-icons">whatshot</i>Third
+
+            <div class="right"><i class="material-icons stroke edit">mode_edit</i><i class="material-icons stroke delete">delete</i></div>
+
+      <div class="right pdefault shadow" style="margin-right:20px;">Estatus: <span style="margin-left:10px;" class="right"> Activo  </span></div>
+
+      <div class="right pdefault shadow" style="margin-right:20px;">Porcentaje: <span style="margin-left:10px;" class="right"> 0%</span> </div>
+
+      </div>
       <div class="collapsible-body colab white-text"><p>Lorem ipsum dolor sit amet.</p></div>
     </li>
   </ul>
--- Modulos/topheader.modulo.php
+++
@@ -1,8 +0,0 @@
-<nav>
-    <div class="nav-wrapper container">
-      <a href="?pg=inicio" class="brand-logo">Demo</a>
-      <ul id="nav-mobile" class="right">
-       <li><a class="button" href="cerrar.php" >Cerrar Sesion</a></li>
-      </ul>
-    </div>
-</nav>
