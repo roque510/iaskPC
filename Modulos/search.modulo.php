02baf67aef15ca4568d5deac6b1407bb012f8457
Armando roque barahona <Armando roque barahona>
2016-10-30 20:37:37 -0600

    front-end 98%

---
+++ Modulos/search.modulo.php
@@ -0,0 +1,12 @@
+<div class=" row container">
+	<div class="box col s12">
+      <form>
+        <div class="input-field">
+         <input id="search" type="search" required>
+          <label for="search"><i class="white-text stroke material-icons">search</i></label>
+          <i class="material-icons">close</i>
+        </div>
+      </form>
+	</div>
+	
+</div>
